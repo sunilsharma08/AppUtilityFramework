e6ff37da194e78e2c0d14a0b3e773370f18f76bd
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-08-22 12:06:24 +0530

    Initial Commit

---
+++ AppUtility/AppUtility.h
@@ -0,0 +1,19 @@
+//
+//  AppUtility.h
+//  AppUtility
+//
+//  Created by Apple on 22/08/16.
+//  Copyright © 2016 Sunil Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppUtility.
+FOUNDATION_EXPORT double AppUtilityVersionNumber;
+
+//! Project version string for AppUtility.
+FOUNDATION_EXPORT const unsigned char AppUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppUtility/PublicHeader.h>
+
+
